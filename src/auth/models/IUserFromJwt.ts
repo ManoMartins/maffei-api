af122001f002ce2ad979b84e33c4aa7303635da0
Manoel Martins <mano.martins29@gmail.com>
2022-04-22 11:52:53 -0300

    :sparkles: implement jwt validation

---
+++ src/auth/models/IUserFromJwt.ts
@@ -0,0 +1,5 @@
+export interface IUserFromJwt {
+  id: string;
+  email: string;
+  name: string;
+}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IUserFromJwt } from '../models/IUserFromJwt';
+import { IUserPayload } from '../models/IUserPayload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: IUserPayload): Promise<IUserFromJwt> {
+    return {
+      id: payload.sub,
+      email: payload.email,
+      name: payload.name,
+    };
+  }
+}
