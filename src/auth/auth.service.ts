e51742deaff107471c2b4a26d5dd57394b40939f
Manoel Martins <mano.martins29@gmail.com>
2022-04-22 10:50:39 -0300

    :sparkles: implement login, guard and localStrategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser(email: string, password: string): any {
+    throw new Error('Method not implemented.');
+  }
+  async session(user) {
+    return {
+      user,
+      token: 'fake-jwt-token',
+    };
+  }
+}
